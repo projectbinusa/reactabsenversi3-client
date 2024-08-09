08abc99a40928c1406324bcdeec6a78ae1d1cf11
fatiyasalsabila123 <117715933+fatiyasalsabila123@users.noreply.github.com>
2024-08-09 14:32:45 +0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const API_DUMMY = "http://localhost:2026";
-// export const API_DUMMY = "api-absenreact.excellentsitem.com";
+// export const API_DUMMY = "http://localhost:2026";
+export const API_DUMMY = "https://api-presensi.excellentsistem.com";
