0bcebcfc92dcecac74dbdc6c1a397e15646786be
DavinaCahyani <davinacahyaniputri@gmail.com>
2024-08-22 13:18:00 +0700

    update perbaikan

--- src/components/SidebarNavbar.js
+++ src/components/SidebarNavbar.js
@@ -168,7 +168,7 @@ function SidebarNavbar() {
         aria-controls="logo-sidebar"
         aria-expanded={isOpen}
         type="button"
-        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-white rounded-lg sm:hidden hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 dark:text-white dark:hover:bg-blue-800 dark:focus:ring-blue-800"
       >
         <span className="sr-only">Open sidebar</span>
         <svg
--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -324,7 +324,7 @@ function Dashboard() {
               </h6>
             </div>
             <hr />
-            <div className="overflow-x-auto shadow-md sm:rounded-lg mt-5">
+            <div className="overflow-x-auto shadow-md sm:rounded-lg mt-8 mb-8">
               <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-left text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
