50f8640ffc189fb1aa874e160623e6da7c915e13
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-09-09 14:04:16 +0700

    update

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,32 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 function PrivateRoute({ element: Component }) {
   const location = useLocation();
 
-  const isTokenExpired = () => {
+  // Fungsi untuk memeriksa apakah token sudah kedaluwarsa atau tidak ada
+  const isTokenValid = () => {
     const token = localStorage.getItem("token");
-    if (!token) return true;
+    if (!token) return false;
     try {
       const tokenData = JSON.parse(atob(token.split(".")[1]));
       const expirationTime = tokenData.exp * 1000;
       const currentTime = Date.now();
-      return currentTime > expirationTime; // Cek apakah token telah
+      return currentTime <= expirationTime;
     } catch (e) {
-      return true;
+      return false;
     }
   };
 
-  // const token = localStorage.getItem("token")
-  // const tokenData = JSON.parse(atob(token.split(".")[1]));
-  // const expirationTime = tokenData.exp * 1000;
-
-  // useEffect(() => {
-  //   console.log("expired: ", expirationTime);
-  // }, []);
-
-  if (isTokenExpired()) {
-    localStorage.removeItem("token");
+  // Jika token tidak valid atau tidak ada, arahkan ke halaman login
+  if (!isTokenValid()) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
 
