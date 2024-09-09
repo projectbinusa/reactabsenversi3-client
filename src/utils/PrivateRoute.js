75c59b3630209d4b5c3331bd2acbe8fdb87965e0
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-09-09 08:02:37 +0700

    update

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -4,27 +4,22 @@ import { Link, Navigate, useLocation } from "react-router-dom";
 function PrivateRoute({ element: Component }) {
   const location = useLocation();
 
-  const decodeBase64Url = (base64Url) => {
-    const base64 = base64Url
-      .replace(/-/g, '+') // mengganti '-' dengan '+'
-      .replace(/_/g, '/'); // mengganti '_' dengan '/'
-    return atob(base64);
-  };
+  // const decodeBase64Url = (base64Url) => {
+  //   const base64 = base64Url
+  //     .replace(/-/g, '+') // mengganti '-' dengan '+'
+  //     .replace(/_/g, '/'); // mengganti '_' dengan '/'
+  //   return atob(base64);
+  // };
 
   const isTokenExpired = () => {
     const token = localStorage.getItem("token");
     if (!token) return true; // Jika token tidak ada, diasumsikan token telah kedaluwarsa
-
-    try {
-      const tokenData = JSON.parse(decodeBase64Url(token.split(".")[1])); // Mengurai bagian data token JWT
-      const expirationTime = tokenData.exp * 1000; // Waktu kedaluwarsa dalam format milidetik
-      const currentTime = Date.now(); // Waktu saat ini dalam format milidetik
-      return currentTime > expirationTime; // Memeriksa apakah token telah kedaluwarsa
-    } catch (error) {
-      console.error("Error decoding token:", error);
-      return true; // Jika ada kesalahan dalam mendekode token, anggap token tidak valid
-    }
+    const tokenData = JSON.parse(atob(token.split('.')[1])); // Mengurai bagian data token JWT
+    const expirationTime = tokenData.exp * 1000; // Waktu kedaluwarsa dalam format milidetik
+    const currentTime = Date.now(); // Waktu saat ini dalam format milidetik
+    return currentTime > expirationTime; // Memeriksa apakah token telah kedaluwarsa
   };
+
 
 
   if (isTokenExpired()) {
