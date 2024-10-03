1bb64eb201a778d516650e06b2a104591ec52087
fatiyasalsabila994 <117715933+fatiyasalsabila123@users.noreply.github.com>
2024-10-03 13:25:21 +0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const API_DUMMY = "http://localhost:2026";
-// export const API_DUMMY = "https://api-presensismpn40.excellentsistem.com";
+// export const API_DUMMY = "http://localhost:2026";
+export const API_DUMMY = "https://api-presensismpn40.excellentsistem.com";
